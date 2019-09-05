f6586f79051015297976ede565ee8047ad74b0a0
MichalGuz <michalguz@gmail.com>
2019-09-05 22:26:43 +0200

    Add annotation @Component to class Triangle.

--- src/main/java/com/spring/basic/start/shape/Triangle.java
+++ src/main/java/com/spring/basic/start/shape/Triangle.java
@@ -1,5 +1,8 @@
 package com.spring.basic.start.shape;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Triangle implements  Shape{
     @Override
     public String getShapeName(){

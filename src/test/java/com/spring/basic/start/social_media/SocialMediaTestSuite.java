27a22e4988c60a63687cdfe9c101e5d44f8aa917
MichalGuz <michalguz@gmail.com>
2019-09-06 21:26:58 +0200

    Create section 'given' in  testGetUserName.

--- src/test/java/com/spring/basic/start/social_media/SocialMediaTestSuite.java
+++ src/test/java/com/spring/basic/start/social_media/SocialMediaTestSuite.java
@@ -1,10 +1,14 @@
 package com.spring.basic.start.social_media;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SocialMediaTestSuite {
     @Test
     public void testGetUserName(){
-
+        // given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.spring.basic.start.social_media");
+        SocialMediaUser user = context.getBean(SocialMediaUser.class);
     }
 }

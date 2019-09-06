fdcfd3936eec3244cf0f80820612676b2a136243
MichalGuz <michalguz@gmail.com>
2019-09-06 20:27:30 +0200

    Create section 'given' in  testCircleLoadedIntoContainer.

--- src/test/java/com/spring/basic/start/SpringRunnerTestSuite.java
+++ src/test/java/com/spring/basic/start/SpringRunnerTestSuite.java
@@ -1,8 +1,12 @@
 package com.spring.basic.start;
 
+import com.spring.basic.start.shape.Circle;
+import com.spring.basic.start.shape.Shape;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,7 +15,9 @@ public class SpringRunnerTestSuite {
 
 	@Test
 	public void testCircleLoadedIntoContainer(){
-
+		// given
+		ApplicationContext context = new AnnotationConfigApplicationContext("com.spring.basic.start");
+		Shape shape = context.getBean(Circle.class);
 	}
 
 }

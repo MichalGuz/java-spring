53eb2ec2c5545556664ff0c58d796d49cc0b084f
MichalGuz <michalguz@gmail.com>
2019-10-04 21:48:22 +0200

    Add back the annotations and imports, because @RunWith(SprongRunner.class) concerns junit4.SpringRunner.

--- src/test/java/com/spring/basic/start/SpringRunnerTestSuite.java
+++ src/test/java/com/spring/basic/start/SpringRunnerTestSuite.java
@@ -6,9 +6,14 @@ import com.spring.basic.start.shape.Square;
 import com.spring.basic.start.shape.Triangle;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SpringRunnerTestSuite {
 
 	@Test

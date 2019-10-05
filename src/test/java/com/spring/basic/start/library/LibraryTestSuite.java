ad1edd90145451a730894fbb8eafb7321b5c8788
MichalGuz <michalguz@gmail.com>
2019-10-05 18:47:03 +0200

    Create 'given' section in testLoadFromDb() with ApplicationContext and Library type variable retrieving bean from container.

--- src/test/java/com/spring/basic/start/library/LibraryTestSuite.java
+++ src/test/java/com/spring/basic/start/library/LibraryTestSuite.java
@@ -3,6 +3,8 @@ package com.spring.basic.start.library;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -10,6 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class LibraryTestSuite {
     @Test
     public void testLoadFromDb() {
-
+        // given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.spring.basic.start.library");
+        Library library = context.getBean(Library.class);
     }
     }

cb4a2b7aa569aa345f540c3aa0e90527ac0ea1d4
MichalGuz <michalguz@gmail.com>
2019-10-05 11:50:06 +0200

    Add annotation @Configuration to class ShapesFactory.

--- src/main/java/com/spring/basic/start/shape/ShapesFactory.java
+++ src/main/java/com/spring/basic/start/shape/ShapesFactory.java
@@ -1,7 +1,9 @@
 package com.spring.basic.start.shape;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ShapesFactory {
     @Bean
     public Square createSquare(){

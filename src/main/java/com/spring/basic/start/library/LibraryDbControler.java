bb95b0806a4fce49c780c82550dd8d13988a262d
MichalGuz <michalguz@gmail.com>
2019-10-05 18:07:37 +0200

    Create methods: saveData() and loadData() in  class LibraryDbControler.

--- src/main/java/com/spring/basic/start/library/LibraryDbControler.java
+++ src/main/java/com/spring/basic/start/library/LibraryDbControler.java
@@ -4,4 +4,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public final class LibraryDbControler {
+    public void saveData() {
+        System.out.println("Saving data to the database.");
+    }
+
+    public void loadData() {
+        System.out.println("Loading data from the data base");
+    }
 }

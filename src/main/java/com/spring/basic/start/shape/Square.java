7affadb9059d1db95ecbab186f167ab53ba24492
MichalGuz <michalguz@gmail.com>
2019-10-05 11:45:15 +0200

    Delete annotation @Component from class Square.

--- src/main/java/com/spring/basic/start/shape/Square.java
+++ src/main/java/com/spring/basic/start/shape/Square.java
@@ -1,8 +1,5 @@
 package com.spring.basic.start.shape;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Square implements Shape{
     @Override
     public String getShapeName(){

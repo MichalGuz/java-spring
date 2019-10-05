133caba9d2e3fe13c7793e995d902c3f09b28a95
MichalGuz <michalguz@gmail.com>
2019-10-05 11:49:29 +0200

    Add annotation @Bean to method createSquare in ShapesFactory.

--- src/main/java/com/spring/basic/start/shape/ShapesFactory.java
+++ src/main/java/com/spring/basic/start/shape/ShapesFactory.java
@@ -1,6 +1,9 @@
 package com.spring.basic.start.shape;
 
+import org.springframework.context.annotation.Bean;
+
 public class ShapesFactory {
+    @Bean
     public Square createSquare(){
         return new Square();
     }

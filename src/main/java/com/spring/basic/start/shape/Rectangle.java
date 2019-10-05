7553c11b90738420860d36f5633f6fb091687048
MichalGuz <michalguz@gmail.com>
2019-10-05 12:02:52 +0200

    Class Rectangle implements interface Shape.

--- src/main/java/com/spring/basic/start/shape/Rectangle.java
+++ src/main/java/com/spring/basic/start/shape/Rectangle.java
@@ -1,4 +1,8 @@
 package com.spring.basic.start.shape;
 
-public class Rectangle {
+public class Rectangle implements Shape{
+    @Override
+    public String getShapeName(){
+        return "This is a rectangle.";
+    }
 }

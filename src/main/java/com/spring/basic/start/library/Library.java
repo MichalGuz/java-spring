a5a32c7625383673896d9737d116dc57caa096ca
MichalGuz <michalguz@gmail.com>
2019-10-05 18:16:16 +0200

    Create ArrayList 'books' and LibraryDbControler fieldd im claaa Library.

--- src/main/java/com/spring/basic/start/library/Library.java
+++ src/main/java/com/spring/basic/start/library/Library.java
@@ -2,6 +2,11 @@ package com.spring.basic.start.library;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public final class Library {
+    private final List<String> books = new ArrayList<>();
+    private final LibraryDbControler libraryDbControler;
 }

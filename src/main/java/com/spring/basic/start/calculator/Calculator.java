fea3d9a89ebf75023e5377de2b49d58f39c9062a
MichalGuz <michalguz@gmail.com>
2019-10-06 11:21:29 +0200

    Create field 'Display display' with annotiation @Autowired and method add() in class Calculator.

--- src/main/java/com/spring/basic/start/calculator/Calculator.java
+++ src/main/java/com/spring/basic/start/calculator/Calculator.java
@@ -1,7 +1,14 @@
 package com.spring.basic.start.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
+    @Autowired
+    Display display;
+
+    public double add(double a, double b) {
+
+    }
 }

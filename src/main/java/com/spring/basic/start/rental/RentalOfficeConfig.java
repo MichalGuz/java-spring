d6983d3f7411633001fa8ca27e074f75e632fa82
MichalGuz <michalguz@gmail.com>
2019-10-06 16:27:47 +0200

    Add annotation @Bean to both methods in class RenatalOfficeConfig.

--- src/main/java/com/spring/basic/start/rental/RentalOfficeConfig.java
+++ src/main/java/com/spring/basic/start/rental/RentalOfficeConfig.java
@@ -1,13 +1,16 @@
 package com.spring.basic.start.rental;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RentalOfficeConfig {
+    @Bean
     public RentalOffice rentalOffice() {
         return new RentalOffice(rentalOfficeDbController());
     }
 
+    @Bean
     public RentalOfficeDbController rentalOfficeDbController() {
         return new RentalOfficeDbController();
     }

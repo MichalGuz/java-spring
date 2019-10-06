6211a2140b9b3134fbd7ab335e0bb2f302332f9b
MichalGuz <michalguz@gmail.com>
2019-10-06 14:21:07 +0200

    Create new ArrayList and field RenatlOfficeDbController in class RentalOffice..

--- src/test/java/com/spring/basic/start/rental/RentalOffice.java
+++ src/test/java/com/spring/basic/start/rental/RentalOffice.java
@@ -2,7 +2,12 @@ package com.spring.basic.start.rental;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class RentalOffice {
+    private final List<String> books = new ArrayList<>();
+    private RentalOfficeDbController rentalOfficeDbController;
 
 }

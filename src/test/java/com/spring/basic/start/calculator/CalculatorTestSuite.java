5441ceb27def3939d2da2960dadaa235100304bd
MichalGuz <michalguz@gmail.com>
2019-10-06 12:38:03 +0200

    Create testCalsulations() in class CalculatorTestSuite.

--- src/test/java/com/spring/basic/start/calculator/CalculatorTestSuite.java
+++ src/test/java/com/spring/basic/start/calculator/CalculatorTestSuite.java
@@ -1,6 +1,7 @@
 package com.spring.basic.start.calculator;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,5 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorTestSuite {
+    @Test
+    public void testCalculations() {
 
+    }
 }

86dbc179b4da3afc9c9bc2ac097c7c90ae28bb4c
MichalGuz <michalguz@gmail.com>
2019-10-08 23:14:24 +0200

    Add @Bean annotation before getReader() method.

--- src/main/java/com/spring/basic/start/reader/ReaderConfig.java
+++ src/main/java/com/spring/basic/start/reader/ReaderConfig.java
@@ -2,6 +2,7 @@ package com.spring.basic.start.reader;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,6 +12,7 @@ public class ReaderConfig {
     @Qualifier("book1")
     Book book;
 
+    @Bean
     public Reader getReader() {
         return new Reader(book);
     }

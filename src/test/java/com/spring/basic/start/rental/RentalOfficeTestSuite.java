7d685c75cb4b74866b1cc3320627c4f45c3aeac4
MichalGuz <michalguz@gmail.com>
2019-10-08 19:36:48 +0200

    Create testLoadDataFromDb() in RentalOfficeTestSuite class.

--- src/test/java/com/spring/basic/start/rental/RentalOfficeTestSuite.java
+++ src/test/java/com/spring/basic/start/rental/RentalOfficeTestSuite.java
@@ -1,5 +1,6 @@
 package com.spring.basic.start.rental;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -7,4 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class RentalOfficeTestSuite {
+    @Test
+    public void testLoadDataFromDb() {
+
+    }
 }

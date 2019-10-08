a93869d26af32e7251b2406e6fb397b35035551f
MichalGuz <michalguz@gmail.com>
2019-10-08 19:14:11 +0200

    Create two void methods loadData() and saveData() in class RentalOfficeDbController.

--- src/main/java/com/spring/basic/start/rental/RenatlOfficeDbController.java
+++ src/main/java/com/spring/basic/start/rental/RenatlOfficeDbController.java
@@ -1,4 +1,11 @@
 package com.spring.basic.start.rental;
 
 public final class RenatlOfficeDbController {
+    public void saveData() {
+        System.out.println("Saving data to the database.");
+    }
+
+    public void loadData() {
+        System.out.println("Load data from the database.");
+    }
 }

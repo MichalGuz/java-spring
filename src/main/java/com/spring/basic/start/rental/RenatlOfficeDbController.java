3aec9419979ac3eb25eeb7a0a1937a6467bfcb9b
MichalGuz <michalguz@gmail.com>
2019-10-08 19:14:38 +0200

    Add annotation @Service to class RentalOfficeDbController.

--- src/main/java/com/spring/basic/start/rental/RenatlOfficeDbController.java
+++ src/main/java/com/spring/basic/start/rental/RenatlOfficeDbController.java
@@ -1,5 +1,8 @@
 package com.spring.basic.start.rental;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public final class RenatlOfficeDbController {
     public void saveData() {
         System.out.println("Saving data to the database.");

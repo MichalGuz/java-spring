b30cb088a72078a6b4cfa04e39afc2a4da55f1dc
MichalGuz <michalguz@gmail.com>
2019-10-08 23:11:11 +0200

    Create field Book type in class ReaderConfig with annotations @Aurowired and @Qualifier.

--- src/main/java/com/spring/basic/start/reader/ReaderConfig.java
+++ src/main/java/com/spring/basic/start/reader/ReaderConfig.java
@@ -1,8 +1,13 @@
 package com.spring.basic.start.reader;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ReaderConfig {
 
+    @Autowired
+    @Qualifier("book1")
+    Book book;
 }

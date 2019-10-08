0b619c4b99a1b8e4d6f3b87cfa37fd7af8b8a2a9
MichalGuz <michalguz@gmail.com>
2019-10-08 19:08:58 +0200

    Create new ArrayList of rented items in class RentalOffice.

--- src/main/java/com/spring/basic/start/rental/RentalOffice.java
+++ src/main/java/com/spring/basic/start/rental/RentalOffice.java
@@ -1,5 +1,9 @@
 package com.spring.basic.start.rental;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class RentalOffice {
+    private final List<String>rentedItems = new ArrayList<>();
 
 }

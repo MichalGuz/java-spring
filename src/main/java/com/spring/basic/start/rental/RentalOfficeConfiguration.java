cfcaf5ab8f7f57b71e90c2449312bb8b55ffe729
MichalGuz <michalguz@gmail.com>
2019-10-08 20:05:56 +0200

    Create RentalOffice type method rentalOfficeConfig() with the call of rentalOfficeDbControllerConfig() method in class RentalOfficeConfiguration.

--- src/main/java/com/spring/basic/start/rental/RentalOfficeConfiguration.java
+++ src/main/java/com/spring/basic/start/rental/RentalOfficeConfiguration.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RentalOfficeConfiguration {
+
+    public RentalOffice rentalOfficeConfig(){
+        return new RentalOffice(rentalOfficeDbControllerConfig());
+    }
+
     public RentalOfficeDbController rentalOfficeDbControllerConfig() {
         return new RentalOfficeDbController();
     }

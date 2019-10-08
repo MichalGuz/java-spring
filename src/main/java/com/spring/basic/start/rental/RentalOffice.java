f6421aa6f78df6ba760372060258078096326421
MichalGuz <michalguz@gmail.com>
2019-10-08 19:06:17 +0200

    Create package 'rental' and class RentalOffice.

--- src/main/java/com/spring/basic/start/rental/RentalOffice.java
+++ src/main/java/com/spring/basic/start/rental/RentalOffice.java
@@ -1,29 +1,5 @@
 package com.spring.basic.start.rental;
 
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service
-public class RentalOffice {
-    private final List<String> books = new ArrayList<>();
-    private RentalOfficeDbController rentalOfficeDbController;
-
-    public RentalOffice(final RentalOfficeDbController rentalOfficeDbController) {
-        this.rentalOfficeDbController = rentalOfficeDbController;
-    }
-
-    public RentalOffice() {
-        // no parameters
-    }
-
-    public void loadDataFromDb() {
-        rentalOfficeDbController.loadData();
-    }
-
-    public void saveDataToDb() {
-        rentalOfficeDbController.saveData();
-    }
+public final class RentalOffice {
 
 }

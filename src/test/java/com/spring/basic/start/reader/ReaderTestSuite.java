d12024c6cf42b87073024130fdef0bd0a2b86f94
MichalGuz <michalguz@gmail.com>
2019-10-08 23:34:21 +0200

    Create 'given' section of testRead(): add ApplicationContext and getBean of Reader.

--- src/test/java/com/spring/basic/start/reader/ReaderTestSuite.java
+++ src/test/java/com/spring/basic/start/reader/ReaderTestSuite.java
@@ -1,10 +1,14 @@
 package com.spring.basic.start.reader;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ReaderTestSuite {
     @Test
     public void testRead(){
-
+        // given
+        ApplicationContext context = new AnnotationConfigApplicationContext(ReaderConfig.class);
+        Reader reader = context.getBean(Reader.class);
     }
 }

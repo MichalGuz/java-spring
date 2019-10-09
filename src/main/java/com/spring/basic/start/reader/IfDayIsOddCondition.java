ba6762f9e928798c4d1aa8d24ba68f60a75c544b
MichalGuz <michalguz@gmail.com>
2019-10-09 18:37:13 +0200

    Class IfDayIsOddCondition implements Condition interface (Spring).

--- src/main/java/com/spring/basic/start/reader/IfDayIsOddCondition.java
+++ src/main/java/com/spring/basic/start/reader/IfDayIsOddCondition.java
@@ -1,4 +1,6 @@
 package com.spring.basic.start.reader;
 
-public class IfDayIsOddCondition {
+import org.springframework.context.annotation.Condition;
+
+public class IfDayIsOddCondition implements Condition {
 }

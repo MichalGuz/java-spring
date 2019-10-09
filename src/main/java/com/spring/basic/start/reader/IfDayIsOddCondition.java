c316eeb37a34e5d6bb9fa036eecbd772cd367ac1
MichalGuz <michalguz@gmail.com>
2019-10-09 18:40:01 +0200

    Create method matches() in class IfDayIsOddCondition.

--- src/main/java/com/spring/basic/start/reader/IfDayIsOddCondition.java
+++ src/main/java/com/spring/basic/start/reader/IfDayIsOddCondition.java
@@ -1,6 +1,12 @@
 package com.spring.basic.start.reader;
 
 import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class IfDayIsOddCondition implements Condition {
+    @Override
+    public boolean mathches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+
+    }
 }

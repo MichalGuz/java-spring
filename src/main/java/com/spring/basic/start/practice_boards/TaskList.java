d475e35d065abaab9e49d92864c0b3a74c4b05c2
MichalGuz <michalguz@gmail.com>
2019-10-09 20:44:28 +0200

    Create consructor of class TaskList.

--- src/main/java/com/spring/basic/start/practice_boards/TaskList.java
+++ src/main/java/com/spring/basic/start/practice_boards/TaskList.java
@@ -1,4 +1,12 @@
 package com.spring.basic.start.practice_boards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskList {
+    private List<String> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
 }

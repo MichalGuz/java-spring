932668aa9721a93009f831253b4cf4d360de48bb
MichalGuz <michalguz@gmail.com>
2019-10-09 20:58:15 +0200

    Create method getTaskListToDo() with annotation @Bean in class BoardConfig.

--- src/main/java/com/spring/basic/start/practice_boards/BoardConfig.java
+++ src/main/java/com/spring/basic/start/practice_boards/BoardConfig.java
@@ -1,7 +1,15 @@
 package com.spring.basic.start.practice_boards;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
+
+    @Bean(name = "taskListToDo")
+    public TaskList getTaskListToDo(){
+        return new TaskList();
+    }
 }

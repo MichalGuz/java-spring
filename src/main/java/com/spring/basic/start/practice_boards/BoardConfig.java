ad01cf3d98bab2a93eb2b746a1eec4901fb951f8
MichalGuz <michalguz@gmail.com>
2019-10-09 21:22:21 +0200

    Add annotation  @Scope(prototype) to method getTaskListToDo() in class BoardConfig.

--- src/main/java/com/spring/basic/start/practice_boards/BoardConfig.java
+++ src/main/java/com/spring/basic/start/practice_boards/BoardConfig.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
 
     @Bean(name = "taskListToDo")
+    @Scope("prototype")
     public TaskList getTaskListToDo() {
         return new TaskList();
     }

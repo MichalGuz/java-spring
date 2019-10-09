f7395aa58f994a81ad6cf2714668b6f3f0f8431f
MichalGuz <michalguz@gmail.com>
2019-10-09 21:31:20 +0200

    Add Board field 'board' with annotation @Autowired in class BoardTestSuite.

--- src/test/java/com/spring/basic/start/practice_boards/BoardTestSuite.java
+++ src/test/java/com/spring/basic/start/practice_boards/BoardTestSuite.java
@@ -1,11 +1,14 @@
 package com.spring.basic.start.practice_boards;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class BoardTestSuite {
-
+    @Autowired
+    private Board board;
 }

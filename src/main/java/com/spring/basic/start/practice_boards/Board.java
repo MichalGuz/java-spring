8046b6e377ea6abd8867e4da7294370bd258f65a
MichalGuz <michalguz@gmail.com>
2019-10-09 21:16:33 +0200

    Create constructor of class Board.

--- src/main/java/com/spring/basic/start/practice_boards/Board.java
+++ src/main/java/com/spring/basic/start/practice_boards/Board.java
@@ -4,4 +4,10 @@ public class Board {
     private final TaskList toDoList;
     private final TaskList inProgressList;
     private final TaskList doneList;
+
+    public Board(TaskList toDoList, TaskList inProgressList, TaskList doneList) {
+        this.toDoList = toDoList;
+        this.inProgressList = inProgressList;
+        this.doneList = doneList;
+    }
 }

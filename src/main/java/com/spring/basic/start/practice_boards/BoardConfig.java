19c451b6ff489a799312deaddfd47c756d45eaf5
MichalGuz <michalguz@gmail.com>
2019-10-09 21:00:32 +0200

    Remove unnecessary imports from class BoardConfig.

--- src/main/java/com/spring/basic/start/practice_boards/BoardConfig.java
+++ src/main/java/com/spring/basic/start/practice_boards/BoardConfig.java
@@ -1,7 +1,5 @@
 package com.spring.basic.start.practice_boards;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 

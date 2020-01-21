8a5ef90a8fb4c11ed7178b8d748428e9b347b0dc
MichalGuz <michalguz@gmail.com>
2020-01-21 23:25:11 +0100

    Create all-args-constructor and getters in class Item.

--- src/main/java/com/spring/good_patterns2/facade/Item.java
+++ src/main/java/com/spring/good_patterns2/facade/Item.java
@@ -3,4 +3,17 @@ package com.spring.good_patterns2.facade;
 public class Item {
     private final Long productId;
     private final double quantity;
+
+    public Item(Long productId, double quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
 }

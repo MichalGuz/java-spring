80f2e98283a4e787b7d6f78d2aba2b0a08e81107
MichalGuz <michalguz@gmail.com>
2020-01-21 23:36:15 +0100

    Create BigDecimal getPrice() method in ProductService class.

--- src/main/java/com/spring/good_patterns2/facade/ProductService.java
+++ src/main/java/com/spring/good_patterns2/facade/ProductService.java
@@ -1,6 +1,13 @@
 package com.spring.good_patterns2.facade;
 
 
+import java.math.BigDecimal;
+import java.util.Random;
+
 public class ProductService {
+    private BigDecimal getPrice(Long productId) {
+        Random generator = new Random();
+        return new BigDecimal(generator.nextInt(100000) / 100);
+    }
 
 }

dfeb405e816ee699c82e96eff4ee2c1a76d9b880
MichalGuz <michalguz@gmail.com>
2020-01-21 23:39:03 +0100

    Add fields of Order class.

--- src/main/java/com/spring/good_patterns2/facade/Order.java
+++ src/main/java/com/spring/good_patterns2/facade/Order.java
@@ -1,5 +1,14 @@
 package com.spring.good_patterns2.facade;
 
-public class Order {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Order {
+    private ProductService productService;
+    private final List<Item> items = new ArrayList<>();
+    private final Long orderId;
+    private final Long userId;
+    private boolean isPaid = false;
+    private boolean isVerified = false;
+    private boolean isSubmitted = false;
 }

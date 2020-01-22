69456a89cb1dc5e56882510e006ea32d573466ad
MichalGuz <michalguz@gmail.com>
2020-01-22 18:48:00 +0100

    Create field of class ShopService.

--- src/main/java/com/spring/good_patterns2/facade/ShopService.java
+++ src/main/java/com/spring/good_patterns2/facade/ShopService.java
@@ -1,4 +1,8 @@
 package com.spring.good_patterns2.facade;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShopService {
+    private final List<Order> orders = new ArrayList<>();
 }

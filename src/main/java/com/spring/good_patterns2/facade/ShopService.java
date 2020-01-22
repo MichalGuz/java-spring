645a72c3fa684d2b605ffafba21f3f3817635321
MichalGuz <michalguz@gmail.com>
2020-01-22 18:48:56 +0100

    Create two another @Autowired fields of class ShopService.

--- src/main/java/com/spring/good_patterns2/facade/ShopService.java
+++ src/main/java/com/spring/good_patterns2/facade/ShopService.java
@@ -1,8 +1,16 @@
 package com.spring.good_patterns2.facade;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShopService {
     private final List<Order> orders = new ArrayList<>();
+
+    @Autowired
+    private Authenticator authenticator;
+
+    @Autowired
+    private ProductService productService;
 }

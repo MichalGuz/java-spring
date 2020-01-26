21d5bbfeffc6072e9f3721ce1e68d64eb0536700
MichalGuz <michalguz@gmail.com>
2020-01-26 23:56:56 +0100

    Create @Autowired Shopservice variable in ShopServiceTestSuite class.

--- src/test/java/com/spring/good_patterns2/facade/ShopServiceTestSuite.java
+++ src/test/java/com/spring/good_patterns2/facade/ShopServiceTestSuite.java
@@ -1,11 +1,13 @@
 package com.spring.good_patterns2.facade;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ShopServiceTestSuite {
-
+    @Autowired
+    private ShopService shopService;
 }

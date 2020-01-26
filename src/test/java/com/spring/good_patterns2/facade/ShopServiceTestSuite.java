c767fd41648d962c7912de0b0a0044a91762ffa8
MichalGuz <michalguz@gmail.com>
2020-01-26 23:55:46 +0100

    Add annotations @RunWith(SpringRunner.class) and @SpringBootTest to ShopServiceTestSuite class.

--- src/test/java/com/spring/good_patterns2/facade/ShopServiceTestSuite.java
+++ src/test/java/com/spring/good_patterns2/facade/ShopServiceTestSuite.java
@@ -1,4 +1,11 @@
 package com.spring.good_patterns2.facade;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ShopServiceTestSuite {
+
 }

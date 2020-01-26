4c70ce0aad6266bee26478e48198ef9b61fc8760
MichalGuz <michalguz@gmail.com>
2020-01-26 23:57:50 +0100

    Create testShopServiceSubmitOrderWithoutFacade() method in ShopServiceTestSuite class.

--- src/test/java/com/spring/good_patterns2/facade/ShopServiceTestSuite.java
+++ src/test/java/com/spring/good_patterns2/facade/ShopServiceTestSuite.java
@@ -1,5 +1,6 @@
 package com.spring.good_patterns2.facade;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ShopServiceTestSuite {
     @Autowired
     private ShopService shopService;
+
+    @Test
+    public void testShopServiceSubmitOrderWithoutFacade(){}
 }

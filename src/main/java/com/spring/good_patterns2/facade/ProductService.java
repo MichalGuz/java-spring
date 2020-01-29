9768bb0fbdba0db515721771b21d08f40f11c331
MichalGuz <michalguz@gmail.com>
2020-01-29 23:41:38 +0100

    Add @Service annotation to class ProductService.

--- src/main/java/com/spring/good_patterns2/facade/ProductService.java
+++ src/main/java/com/spring/good_patterns2/facade/ProductService.java
@@ -1,9 +1,12 @@
 package com.spring.good_patterns2.facade;
 
 
+import org.springframework.stereotype.Service;
+
 import java.math.BigDecimal;
 import java.util.Random;
 
+@Service
 public class ProductService {
     public BigDecimal getPrice(Long productId) {
         Random generator = new Random();

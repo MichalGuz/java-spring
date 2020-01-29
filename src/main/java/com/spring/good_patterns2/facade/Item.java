a4b80f71d6d96d0bedcc15ae01af61a4bd4f5a8b
MichalGuz <michalguz@gmail.com>
2020-01-29 23:57:48 +0100

    Remove field 'price' from constructor and getters of class Item and from that class.

--- src/main/java/com/spring/good_patterns2/facade/Item.java
+++ src/main/java/com/spring/good_patterns2/facade/Item.java
@@ -3,12 +3,10 @@ package com.spring.good_patterns2.facade;
 public class Item {
     private final Long productId;
     private final double quantity;
-    private final double price;
 
-    public Item(Long productId, double quantity, double price) {
+    public Item(Long productId, double quantity) {
         this.productId = productId;
         this.quantity = quantity;
-        this.price = price;
     }
 
     public Long getProductId() {
@@ -17,9 +15,5 @@ public class Item {
 
     public double getQuantity() {
         return quantity;
-    }
-
-    private double getPrice() {
-        return price;
     }
 }

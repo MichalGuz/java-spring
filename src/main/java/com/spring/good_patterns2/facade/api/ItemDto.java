bf0e661a771984107568b42c7160f145bb799a59
MichalGuz <michalguz@gmail.com>
2020-01-31 00:01:55 +0100

    Create constructor of ItemDto class.

--- src/main/java/com/spring/good_patterns2/facade/api/ItemDto.java
+++ src/main/java/com/spring/good_patterns2/facade/api/ItemDto.java
@@ -4,4 +4,8 @@ public final class ItemDto {
     private final Long productId;
     private final double quantity;
 
+    public ItemDto(final Long productId, final double quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
 }

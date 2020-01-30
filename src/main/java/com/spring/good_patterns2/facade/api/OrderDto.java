9e7faea9965f9365a36a3b1336577a471f959142
MichalGuz <michalguz@gmail.com>
2020-01-31 00:11:55 +0100

    Create a field for collectin of Items in OrderDto class.

--- src/main/java/com/spring/good_patterns2/facade/api/OrderDto.java
+++ src/main/java/com/spring/good_patterns2/facade/api/OrderDto.java
@@ -1,4 +1,11 @@
 package com.spring.good_patterns2.facade.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class OrderDto {
+    private final class OrderDto {
+        private final List<ItemDto> items = new ArrayList<>();
+
+    }
 }

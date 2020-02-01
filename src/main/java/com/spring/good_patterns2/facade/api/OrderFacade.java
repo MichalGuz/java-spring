ad815a9d84a0eee315c67b5ebb60da9a797a643e
MichalGuz <michalguz@gmail.com>
2020-02-01 22:51:45 +0100

    Import org.slf4j.Logger and add call method getLogger() in declaration of static Logger variable in OrderFacade class.

--- src/main/java/com/spring/good_patterns2/facade/api/OrderFacade.java
+++ src/main/java/com/spring/good_patterns2/facade/api/OrderFacade.java
@@ -1,13 +1,13 @@
 package com.spring.good_patterns2.facade.api;
 
 import com.spring.good_patterns2.facade.ShopService;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.logging.Logger;
 
 public final class OrderFacade {
     @Autowired
     private ShopService shopService;
-    private static final Logger LOGGER = LoggerFactory(OrderFacade.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
 }

44841437029d330d04bb53ad98eec5216e83378c
MichalGuz <michalguz@gmail.com>
2020-02-01 22:48:38 +0100

    Create static final Logger variable in OrderFacade class.

--- src/main/java/com/spring/good_patterns2/facade/api/OrderFacade.java
+++ src/main/java/com/spring/good_patterns2/facade/api/OrderFacade.java
@@ -1,9 +1,13 @@
 package com.spring.good_patterns2.facade.api;
 
 import com.spring.good_patterns2.facade.ShopService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.logging.Logger;
 
 public final class OrderFacade {
     @Autowired
     private ShopService shopService;
+    private static final Logger LOGGER = LoggerFactory(OrderFacade.class);
 }
